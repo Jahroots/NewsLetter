21c3978f9f60a9320bb8fe6f71028ce0148d2d1c
Jahroots <jahroots972@gmail.com>
2011-09-30 01:23:38 +0200

    Externalize string for equals assertion

--- src/test/java/com/newsletter/test/HibernateUtilTestCase.java
+++ src/test/java/com/newsletter/test/HibernateUtilTestCase.java
@@ -1,24 +1,23 @@
 package com.newsletter.test;
 
+import com.newsletter.model.Users;
+import com.newsletter.util.HibernateUtil;
 import junit.framework.Assert;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 
-import com.newsletter.model.Users;
-import com.newsletter.util.HibernateUtil;
-
 public class HibernateUtilTestCase {
 
 	private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-	
-	@Test
+    private static final String FAKE_USER = "fakeUser";
+
+    @Test
 	public void session(){
 		Assert.assertNotNull(sessionFactory);
 		
 		Users user = new Users();
-		user.setUser("Rejeb");
+		user.setUser(FAKE_USER);
 		
 		Session session = sessionFactory.openSession();
 		session.save(user);
@@ -26,6 +25,7 @@ public class HibernateUtilTestCase {
 		
 		final Users u = (Users) session.get(Users.class, 1);
 		Assert.assertNotNull(u);
+        Assert.assertEquals(FAKE_USER, u.getUser());
 		session.close();
 	}
 	
